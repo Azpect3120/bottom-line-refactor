313fe0b1ebc3d02cedc25b4624cea76e57739f59
Azpect3120 <hhargreaves2006@gmail.com>
2023-09-15 21:07:14 -0700

    Created updates.ejs and finished basic dev portal setup. Search functionality does not work.

--- routes/devPortal.js
+++ routes/devPortal.js
@@ -33,8 +33,12 @@ router.get("/dev-portal", (req, res) => {
                     // Get update list from database
                     let updates = result.rows;
 
-                    console.log(updates);
-                    
+                    // Sort updates
+                    updates = updates.sort((a, b) => {
+                        a = new Date(a.timestamp);
+                        b = new Date(b.timestamp);
+                        return b - a;
+                    });
 
                     // Render the page with the updates
                     res.status(200).render("dashboard/devPortal", { user, updates });
@@ -43,8 +47,64 @@ router.get("/dev-portal", (req, res) => {
         
         } else {
             // Redirect user that doesn't have permission
+            res.status(401).redirect("/dash");
         }
 
+    } else {
+        res.status(302).redirect("/login");
+    }
+});
+
+// '/dev-portal/view?id=<id>' : Displays detailed information about an database update 
+router.get("/dev-portal/view", (req, res) => {
+    // Get the query body
+    const update_id = req.query.id;
+
+    // Get user objet from session
+    const user = req.session.user;
+
+    // Ensure user exists
+    if (user) {
+        // Ensure user has dev permissions
+        if (user.permission.toLowerCase() === "dev")  {
+            // Query database for update
+            database.query("SELECT * FROM updates WHERE id = $1", [ update_id ], (err, result) => {
+                // Error
+                if (err) {
+                    console.error("Error fetching update details", err);
+                    res.status(500).redirect("/dash/dev-portal");
+                } else {
+                    // Get update from the query
+                    const update = result.rows[0];
+
+                    // Query database for user name
+                    database.query("SELECT * FROM users WHERE id = $1", [ update.user_id ], (err, result) => {
+                        // Error
+                        if (err) {
+                            console.error("Error fetching update username", err);
+                            res.status(500).redirect("/dash/dev-portal");
+                        } else {
+                            // Update the username on the update object
+                            update.username = result.rows[0].username;
+
+                            // Reformat argument array
+                            const args = update.arguments.split(",");
+                            for (let i = 0; i < args.length; i++) {
+                                args[i] = `$${i+1} : ${args[i]}`;
+                            }
+                            update.arguments = args.join("<br>");
+
+                            // Render page with data
+                            res.status(200).render("dashboard/update", { user, update })
+                        }
+                    });
+                }
+            });
+        } else {
+            // Redirect user that doesn't have permission
+            res.status(401).redirect("/dash");
+        }
+       
     } else {
         res.status(302).redirect("/login");
     }
