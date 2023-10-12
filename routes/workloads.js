5d0519c66718d46443c25055ac6949dbdca1468d
Azpect3120 <hhargreaves2006@gmail.com>
2023-10-11 22:34:25 -0700

    FEAT: Working on sales tax workload page.

--- routes/workloads.js
+++ routes/workloads.js
@@ -52,6 +52,28 @@ router.get("/workloads/sales-tax", (req, res) => {
     }
 });
 
+router.post("/workloads/sales-tax/update", (req, res) => {
+    // Get user from session
+    const user = req.session.user;
+
+    // Ensure user is logged in 
+    if (user) {
+        // Get form data
+        const form = req.body;
+
+        res.status(200).json(form);
+
+    
+
+
+
+
+        // res.status(200).render("dashboard/workloads/salesTax", { user });
+    } else {
+        res.status(302).redirect("/login");
+    }
+});
+
 
 
 // Export router
