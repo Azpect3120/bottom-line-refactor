deafa714a4372b6ed138536f46a952be4b061c8e
Azpect3120 <hhargreaves2006@gmail.com>
2023-10-20 23:11:42 -0700

    FEAT: Added creation menu for sales tax workload page

--- routes/workloads.js
+++ routes/workloads.js
@@ -60,18 +60,35 @@ router.get("/workloads/sales-tax", (req, res) => {
             current_year: new Date().getFullYear()
         }
 
+        // Get list of clients
+        let clients = [];
+
         // SQL
-        const query = "SELECT * FROM salestaxworkload INNER JOIN clients ON salestaxworkload.client_id = clients.id WHERE year = $1";
-        const args = [ year ];
+        let query = "SELECT * FROM clients";
+        let args = [];
+
+        database.query(query, args, (err, result) => {
+            if (err) {
+                console.error("Error fetching client list", err);
+                res.status(500).render("/dashboard/workloads/salesTax", { user, data, clients });
+                return
+            } else {
+                clients = result.rows;
+            }
+        });
+
+
+        // SQL
+        query = "SELECT * FROM salestaxworkload INNER JOIN clients ON salestaxworkload.client_id = clients.id WHERE year = $1";
+        args = [ year ];
 
         database.query(query, args, (err, result) => {
             if (err) {
                 console.error("Error fetching workload", err);
-                res.status(500).render("dashboard/workloads/salesTax", { user, data });
+                res.status(500).render("dashboard/workloads/salesTax", { user, data, clients });
             } else {
                 data.clients = search != "" ? result.rows.filter(client => client.name.includes(search)) : result.rows;
-                console.log(data);
-                res.status(200).render("dashboard/workloads/salesTax", { user, data });
+                res.status(200).render("dashboard/workloads/salesTax", { user, data, clients });
             }
         });
     } else {
@@ -95,7 +112,7 @@ router.post("/workloads/sales-tax/update", (req, res) => {
         const query = "UPDATE salestaxworkload SET frequency = $1, " +
             "jan = $2, feb = $3, mar = $4, apr = $5, may = $6, jun = $7, " +
             "jul = $8, aug = $9, sep = $10, oct = $11, nov = $12, dec = $13 " +
-            "WHERE id = $14";
+            "WHERE client_id = $14";
         const args = [
             form.frequency,
             form.jan,
@@ -130,6 +147,50 @@ router.post("/workloads/sales-tax/update", (req, res) => {
 });
 
 
+// 'workloads/sales-tax/create' : Add a user into the workload database
+router.post("/workloads/sales-tax/create", (req, res) => {
+    // Get user from session
+    const user = req.session.user;
+
+    // Get form info
+    const { client_name, frequency, year } = req.body;
+
+    // SQL
+    let query = "SELECT id FROM clients WHERE name ILIKE $1";
+    let args = [ client_name ];
+
+    // Get client id
+    database.query(query, args, (err, result) => {
+        if (err) {
+            console.error("Error fetching user id", err);
+            res.status(500).redirect("/dash/workloads/sales-tax");
+            return
+        } else {
+            if (result.rows[0]) {
+                // SQL
+                query = "INSERT INTO salestaxworkload (client_id, frequency, year, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec) " + 
+                        "VALUES ($1, $2, $3, 'not-checked', 'not-checked', 'not-checked', 'not-checked', 'not-checked', 'not-checked', 'not-checked', " +
+                        "'not-checked', 'not-checked', 'not-checked', 'not-checked', 'not-checked');";
+                args = [ result.rows[0].id, frequency, year ];
+
+                // Insert into database
+                database.query(query, args, (err, result) => {
+                    if (err) {
+                        console.error("Error adding new client to workload", err);
+                        res.status(500).redirect("/dash/workloads/sales-tax");
+                    } else {
+                        updateDatabase(user.id, "salestaxworkload", query, args, result);
+                        res.status(201).redirect("/dash/workloads/sales-tax");
+                    }
+                });
+            } else {
+                res.status(400).redirect("/dash/workloads/sales-tax");
+                return
+            }
+        }
+    });
+
+});
 
 // Export router
 module.exports = router;
